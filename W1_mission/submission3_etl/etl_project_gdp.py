48cc809f1732ee912fb4dbadf04d5b686d19eb4d
patrashu <qhdrmfdl123@naver.com>
2024-07-03 18:34:48 +0900

    Add W1_mission/submission3/etl_project_gdp.py

---
+++ W1_mission/submission3_etl/etl_project_gdp.py
@@ -0,0 +1,183 @@
+import os
+import sys
+import json
+from datetime import datetime
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+
+class Logger:
+    """Logger Class"""
+    def __init__(self, log_file: str | os.PathLike) -> None:
+        """__init__ Constructor
+        
+        Args:
+            log_file (str | os.PathLike): _description_
+        Returns:
+            None
+        """
+        self.log_file = log_file
+
+    def logging(
+        self, 
+        msg: str, 
+        level: str = "INFO"
+    ) -> None:
+        """Logging Function
+        
+        Args:
+            msg (str): _description_
+            level (str, optional): _description_. Defaults to "INFO".
+        Returns:
+            None
+        """
+        timestamp = datetime.now().strftime('%Y-%B-%d %H:%M:%S')
+        with open(self.log_file, 'a') as f:
+            line = f"{timestamp} - {level} - {msg}\n"
+            f.write(line)
+            print(line)
+    
+
+def extract(
+    url: str,
+    json_path: str | os.PathLike,
+    logger: Logger
+) -> bool:
+    """Extract Table Data from Wikipedia and Save as JSON
+    
+    Args:
+        url (str): Wikipedia URL
+        json_path (str, os.PathLike): JSON filepath to save
+        logger (Logger): Logger
+    Returns:
+        success_flag (bool): Success Flag
+    """
+    success_flag = True
+    logger.logging(f"Extracting data from {url}", "INFO")
+    try:
+        html = requests.get(url)
+        soup = bs(html.content, 'html.parser')
+        table = soup.find('table', {'class': 'wikitable'})
+
+        # Country/Territory: [other data]
+        data = {}
+        for i, row in enumerate(table.find_all('tr')):
+            # Remove Title
+            if i < 3:
+                continue
+
+            # Extract each column
+            cols = row.find_all(['th', 'td'])
+            cols = [col.text.strip() for col in cols]
+            data[cols[0]] = cols[1:]
+        logger.logging(f"Finished extracting data from {url}", "INFO")
+
+        with open(json_path, 'w') as f:
+            json.dump(data, f)
+        logger.logging(f"Data is saved as {json_path}", "INFO")
+
+    except Exception as e:
+        logger.logging("Failed to extract data from Wikipedia" + e, "ERROR")
+        print(e)
+        success_flag = False
+
+    return success_flag
+
+
+def transform(
+    json_path: str | os.PathLike,
+    df_path: str | os.PathLike,
+    logger: Logger,
+) -> bool:
+    """Transform Data
+    
+    Args:
+        json_path (str | os.PathLike): JSON filepath
+        df_path (str | os.PathLike): DataFrame filepath
+        logger (Logger): Logger
+    Returns:
+        success_flag (bool): Success Flag
+    """
+    success_flag = True
+    logger.logging("Transforming data", "INFO")
+    try:
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+
+        # Preprocessing
+        new_data = []
+        for country, values in data.items():
+            gdp = values[0]
+            year = values[1]
+            if gdp == '—':
+                gdp, year = None, None
+            else:
+                gdp = float(gdp.replace(',', ''))
+                gdp = round(gdp*0.001, 2) # million to billion
+                if len(year) > 4:
+                    year = int(year[-4:])
+            new_data.append([country, gdp, year])
+
+        df = pd.DataFrame(
+            new_data,
+            columns=[
+                "Country/Territory",
+                "GDP(US$MM)",
+                "Year"
+            ],
+        )
+        df.to_csv(df_path, index=False)
+        logger.logging(f"DataFrame is saved as {df_path}", "INFO")
+
+    except Exception as e:
+        logger.logging("Failed to transform data", "ERROR")
+        print(e)
+        success_flag = False
+        
+    return success_flag   
+
+
+def load(
+    df_path: str | os.PathLike,
+    logger: Logger,
+)-> None:
+    """Load Dataframe and print results
+    
+    Args:
+        df_path (str | os.PathLike): DataFrame filepath
+        logger (Logger): Logger
+    Returns:
+        None
+    """
+    
+    logger.logging("Loading data", "INFO")
+    df = pd.read_csv(df_path)
+    upper_100b = df[df['GDP(US$MM)'] > 100]
+    print(upper_100b)
+    
+    
+    
+    logger.logging("Finished loading data", "INFO")
+
+
+if __name__ == '__main__':
+    LOGGER_PATH = "etl_project_log.txt"
+    WIKI_URL = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
+    JSON_PATH = "Countries_by_GDP.json"
+    DF_PATH = "Countries_by_GDP.csv"
+
+    logger = Logger(LOGGER_PATH)
+    logger.logging("ETL Process is Started", "INFO")
+    
+    ext_flag = extract(WIKI_URL, JSON_PATH, logger)
+    if not ext_flag:
+        sys.exit(0)
+
+    trf_flag = transform(JSON_PATH, DF_PATH, logger)
+    if not trf_flag:
+        sys.exit(0)
+
+    load(DF_PATH, logger)
+    logger.logging("ETL Process is Finished", "INFO")
