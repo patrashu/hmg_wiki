c439b2bcd9443bc34bfd1e0c5a0dc8cdc00f4951
patrashu <qhdrmfdl123@naver.com>
2024-07-04 16:40:24 +0900

    Modified query and Add etl_project_gdp_imf_official.py

--- W1_mission/submission3_etl/etl_project_gdp_imf_official.py
+++ W1_mission/submission3_etl/etl_project_gdp_imf_official.py
@@ -1,12 +1,13 @@
 import os
 import sys
+import time
 import json
 import sqlite3
 from datetime import datetime
 
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class Logger:
@@ -40,7 +41,7 @@ class Logger:
             f.write(line)
             print(line)
     
-
+    
 def make_region_df(
     raw_txt_path: str | os.PathLike,
     region_df_path: str | os.PathLike
@@ -67,79 +68,116 @@ def make_region_df(
 
     df = pd.DataFrame(data=data)
     df.to_csv(region_df_path, index=False)
-    
 
-def extract(
+
+def extract_imf_official(
     url: str,
     region_df_path: str | os.PathLike,
+    year: int,
     json_path: str | os.PathLike,
     logger: Logger
-) -> bool:
+):
     """Extract Table Data from Wikipedia and Save as JSON
     
     Args:
         url (str): Wikipedia URL
         region_df_path (str, os.PathLike): Region DataFrame filepath
+        year (int): Year
         json_path (str, os.PathLike): JSON filepath to save
         logger (Logger): Logger
     Returns:
         success_flag (bool): Success Flag
     """
-    success_flag = True
-    logger.log(f"Extracting data from {url}", "INFO")
     
+    success_flag = True
+    logger.log(f"Extracting data from IMF Official: {url}")
     region_df = pd.read_csv(region_df_path)
+    
     try:
-        html = requests.get(url)
-        soup = bs(html.content, 'html.parser')
-        table = soup.find('table', {'class': 'wikitable'})
-
-        # Country/Territory: [other data]
+        # set driver
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+        
+        # get data
+        driver.get(url)
+        time.sleep(20)
+        
         data = {}
-        for i, row in enumerate(table.find_all('tr')):
-            # Remove Title
-            if i < 3:
-                continue
+        i = 1
+        while True:
+            try:
+                xpath = f'//*[@id="dm-main"]/div[1]/radio-group/div[3]/imf-ranking/radio-group/div[2]/div[2]/div[{i}]/div'
+                country, gdp = driver.find_element(By.XPATH, xpath).text.split('\n')
+                if gdp == 'no data':
+                    pass
+                else:
+                    gdp = gdp.split()
+                    if len(gdp) == 1:
+                        gdp = float(gdp[0])
+                    else:
+                        gdp = float(gdp[0]) * 1000
+                    
+                    # get region of the country
+                    region = None
+                    try:
+                        region = region_df[region_df["Country"] == country]["Region"].values[0]
+                    except:
+                        tmp = None
+                        for coun in region_df["Country"]:
+                            if coun in country:
+                                tmp = coun
+                                break
+                        if tmp:
+                            region = region_df[region_df["Country"] == tmp]["Region"].values[0]
+                        else:
+                            region = "Unknown"
+                    finally:
+                        data[country] = [gdp, year, region]
+                i += 1
+            except:
+                break
 
-            # Extract each column
-            cols = row.find_all(['th', 'td'])
-            cols = [col.text.strip() for col in cols]
-            region = region_df[region_df['Country'] == cols[0]]['Region'].values[0] if cols[0] in region_df['Country'].values else 'N/A'                                     
-            data[cols[0]] = cols[1:] + [region]
-        logger.log(f"Finished extracting data from {url}", "INFO")
-
-        with open(json_path, 'w') as f:
+        driver.quit()
+        logger.log("Data Extracted Successfully", "INFO")
+        
+        with open(str(year) + json_path, 'w') as f:
             json.dump(data, f)
         logger.log(f"Data is saved as {json_path}", "INFO")
-
+            
     except Exception as e:
-        logger.log("Failed to extract data from Wikipedia", "ERROR")
+        logger.log("Failed to extract data from IMF Official", "ERROR")
         print(e)
         success_flag = False
 
     return success_flag
-
-
+    
+    
 def transform_to_dbtable(
     json_path: str | os.PathLike,
+    year: int,
     db_path: str | os.PathLike,
-    logger: Logger,
+    logger: Logger
 ) -> bool:
-    """Transform Data to database table
+    """Transform JSON Data to Database Table
     
     Args:
         json_path (str | os.PathLike): JSON filepath
+        year (int): Year
         db_path (str | os.PathLike): Database filepath
         logger (Logger): Logger
     Returns:
         success_flag (bool): Success Flag
     """
+    
     success_flag = True
-    logger.log("Transforming JSON to DB Table", "INFO")
+    logger.log("Transforming JSON Data to Database Table", "INFO")
     try:
+        json_path = str(year) + json_path
+        db_path = str(year) + db_path
+        
         with open(json_path, 'r') as f:
             data = json.load(f)
-        
+            
         # check db file exists
         if os.path.exists(db_path):
             return success_flag
@@ -161,42 +199,41 @@ def transform_to_dbtable(
             gdp, year, region = values[0], values[1], values[-1]
             if gdp == '—':
                 continue
-            gdp = float(gdp.replace(',', ''))
-            gdp = round(gdp*0.001, 2) # million to billion
-            if len(year) > 4:
-                year = year[-4:]
+            gdp = round(gdp, 2) # million to billion
             conn.execute(
                 "INSERT INTO WorldEconomies VALUES (?, ?, ?, ?)",
                 (country, gdp, year, region)
             )
         conn.commit()
         logger.log(f"DB Table is saved as {db_path}", "INFO")
-
+    
     except Exception as e:
         logger.log("Failed to transform data", "ERROR")
         print(e)
         success_flag = False
         os.remove(db_path)
-        
-    return success_flag   
+   
+    return success_flag
 
 
 def load(
     db_path: str | os.PathLike,
-    logger: Logger,
+    year: int,
+    logger: Logger
 ) -> bool:
-    """Load Database and print results
+    """Load Data from Database
     
     Args:
         db_path (str | os.PathLike): Database filepath
+        yead (int): Year
         logger (Logger): Logger
     Returns:
         success_flag (bool): Success Flag
     """
-    
-    logger.log("Loading Data from DB Table", "INFO")
     success_flag = True
+    logger.log("Loading Data from Database", "INFO")
     try:
+        db_path = str(year) + db_path
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
         
@@ -237,36 +274,38 @@ def load(
         
         logger.log("All Queries are Finished", "INFO")
         logger.log('Done!!', 'INFO')
-
+        
     except Exception as e:
-        logger.log("Failed to Query from DB Table", "ERROR")
+        logger.log("Failed to load data", "ERROR")
         print(e)
         success_flag = False
-
+    
     return success_flag
-        
 
+
+  
 if __name__ == '__main__':
     LOGGER_PATH = "etl_project_log.txt"
     RAW_TXT_PATH = "region.txt"
     REGION_DF_PATH = "region.csv"
-    WIKI_URL = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
-    JSON_PATH = "Countries_by_GDP.json"
-    DB_PATH = "Countries_by_GDP.db"
-
+    YEAR = 2024
+    IMF_URL = f"https://www.imf.org/external/datamapper/NGDPD@WEO/OEMDC/ADVEC/WEOWORLD?year={YEAR}"
+    JSON_PATH = "imf_official.json"
+    DB_PATH = "imf_official.db"
+    
     logger = Logger(LOGGER_PATH)
-    logger.log("ETL Process is Started", "INFO")
+    logger.log("ETL Process Started", "INFO")
     
     make_region_df(RAW_TXT_PATH, REGION_DF_PATH)
-    ext_flag = extract(WIKI_URL, REGION_DF_PATH, JSON_PATH, logger)
+    ext_flag = extract_imf_official(IMF_URL, REGION_DF_PATH, YEAR, JSON_PATH, logger)
     if not ext_flag:
         sys.exit(0)
 
-    trf_flag = transform_to_dbtable(JSON_PATH, DB_PATH, logger)
+    trf_flag = transform_to_dbtable(JSON_PATH, YEAR, DB_PATH, logger)
     if not trf_flag:
         sys.exit(0)
 
-    load_flag = load(DB_PATH, logger)
+    load_flag = load(DB_PATH, YEAR, logger)
     if not load_flag:
         sys.exit(0)
     logger.log("ETL Process is Finished", "INFO")
