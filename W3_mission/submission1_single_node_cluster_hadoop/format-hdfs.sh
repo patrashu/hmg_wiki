add4941c2c5f30f26dcfafcc71e61ee9bc4d2b0b
patrashu <qhdrmfdl123@naver.com>
2024-07-16 23:03:17 +0900

    Modified Hadoop Single Node Cluster Dockerfile

--- W3_mission/submission1_single_node_cluster_hadoop/format-hdfs.sh
+++ W3_mission/submission1_single_node_cluster_hadoop/format-hdfs.sh
@@ -1,25 +1,27 @@
 
-# HDFS 포맷 (첫 실행 시에만 필요)
-if [ ! -d "/opt/hadoop/hdfs/namenode/current" ]; then
-  echo "Formatting HDFS namenode..."
-  /opt/hadoop/bin/hdfs namenode -format -force -nonInteractive
-fi
+su - hdfs -c '
+  export HADOOP_COMMON_LIB_NATIVE_DIR=$/opt/hadoop/lib/native$
+  if [ ! -d "/opt/hadoop/hdfs/namenode/current" ]; then
+    echo "Formatting HDFS namenode..."
+    /opt/hadoop/bin/hdfs namenode -format -force -nonInteractive
+  fi
+  
+  echo "Starting HDFS namenode..."
+  /opt/hadoop/sbin/hadoop-daemon.sh start namenode
 
-echo "Starting HDFS namenode..."
-/opt/hadoop/sbin/hadoop-daemon.sh start namenode
+  echo "Starting HDFS datanode..."
+  /opt/hadoop/sbin/hadoop-daemon.sh start datanode
 
-echo "Starting HDFS datanode..."
-/opt/hadoop/sbin/hadoop-daemon.sh start datanode
+  echo "Starting HDFS secondary namenode..."
+  /opt/hadoop/sbin/hadoop-daemon.sh start secondarynamenode
+'
 
-echo "Starting YARN resource manager..."
-/opt/hadoop/sbin/yarn-daemon.sh start resourcemanager
+su - yarn -c '
+  echo "Starting YARN ResourceManager..."
+  /opt/hadoop/sbin/yarn-daemon.sh start resourcemanager
 
-echo "Starting YARN node manager..."
-/opt/hadoop/sbin/yarn-daemon.sh start nodemanager
-
-echo "Starting HDFS secondary namenode..."
-/opt/hadoop/sbin/hadoop-daemon.sh start secondarynamenode
+  echo "Starting YARN NodeManager..."
+  /opt/hadoop/sbin/yarn-daemon.sh start nodemanager
+'
 
 tail -f /opt/hadoop/logs/*.log
-
-/usr/sbin/sshd -D
